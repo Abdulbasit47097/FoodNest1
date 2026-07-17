741357778b8dbfc633c7decde79f3c4053b395cc
Abdul Basit <abdul47097@gmail.com>
2026-07-17 14:51:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World and wellcome to the progrmaing world");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 3000");
+
+
+    const simpleArray =
+        [7, 8, 9];
+
+    const matrix = [
+        [1, 2, 3], // Row 0
+        [4, 5, 6], // Row 1
+        [7, 8, 9]  // Row 2
+    ];
+
+    console.log(matrix[1][2]);
+});
+
